97d1c1a091eb057ab68fd563a6a8b84c8994897e
tancoc <109244972+tancoc@users.noreply.github.com>
2022-11-11 15:25:14 +0800

    Update index.jsx

--- layouts/header/index.jsx
+++ layouts/header/index.jsx
@@ -1,123 +1,115 @@
+import { useState, useEffect } from 'react'
 import NextLink from 'next/link'
 import { useRouter } from 'next/router'
 import { useSession, signIn, signOut } from 'next-auth/react'
-import { Avatar, Button, chakra, Flex, Grid, GridItem, Icon, IconButton, Link, Text } from '@chakra-ui/react'
-import { FiArchive, FiBox, FiBriefcase, FiCreditCard, FiEdit2, FiGrid, FiLogOut, FiPieChart, FiSettings, FiStar, FiUsers } from 'react-icons/fi'
+import { Avatar, Button, chakra, Flex, Icon, IconButton, Link, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Text, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import { FiBox, FiLogOut, FiMenu, FiMoon, FiSun } from 'react-icons/fi'
 
-const Sidebar = ({ isSidebarOpen, onSidebarClose }) => {
+const Header = ({ onSidebarOpen }) => {
 	const router = useRouter()
 	const { data: session } = useSession()
+	const { toggleColorMode } = useColorMode()
+	const colorModeIcon = useColorModeValue(<FiMoon size={16} fill="currentColor" />, <FiSun size={16} fill="currentColor" />)
+	const [isScrolling, setIsScrolling] = useState(false)
 
-	const links = {
-		admin: [
-			{ pathname: 'dashboard', icon: FiPieChart },
-			{ pathname: 'blogs', icon: FiEdit2 },
-			{ pathname: 'units', icon: FiGrid },
-			{ pathname: 'tenants', icon: FiUsers },
-			{ pathname: 'companies', icon: FiBriefcase },
-			{ pathname: 'payments', icon: FiCreditCard },
-			{ pathname: 'accounts', icon: FiStar },
-			{ pathname: 'archive', icon: FiArchive },
-			{ pathname: 'settings', icon: FiSettings }
-		],
-		user: [
-			{ name: 'my profile', pathname: 'profile' },
-			{ name: 'settings', pathname: 'settings' }
-		],
-		default: [
-			{ name: 'home', pathname: '' },
-			{ name: 'blogs', pathname: 'blogs' },
-			{ name: 'services', pathname: 'services' },
-			{ name: 'company', pathname: 'company' },
-			{ name: 'call us', pathname: 'contact' }
-		]
-	}
+	useEffect(() => {
+		if (typeof window !== 'undefined') {
+			window.addEventListener('scroll', () => {
+				setIsScrolling(window.pageYOffset > 0)
+			})
+		}
+	}, [])
 
 	return (
-		<>
-			<chakra.div bg="hsla(0, 0%, 0%, 0.4)" position="fixed" top={0} left={0} h="100vh" w="full" visibility={isSidebarOpen ? 'visible' : 'hidden'} opacity={isSidebarOpen ? 1 : 0} transition="0.4s ease-in-out" zIndex={99} onClick={onSidebarClose} />
+		<chakra.header bg="white" position="sticky" top={0} shadow={isScrolling && 'sm'} transition=".4s" zIndex={99} _dark={{ bg: isScrolling ? 'surface' : 'system', border: 'none', shadow: isScrolling && 'dark-xl' }}>
+			<Flex justify="space-between" align="center" gap={6} mx="auto" px={6} h={20} w="full" maxW={1280}>
+				<Flex justify="start" align="center" gap={3}>
+					{session && session.user.role === 'Admin' && <IconButton display={{ base: 'none', md: 'flex' }} variant="outline" color="accent-1" icon={<FiMenu size={16} />} onClick={onSidebarOpen} />}
 
-			<chakra.aside bg="white" position="fixed" top={0} left={isSidebarOpen ? 0 : -256} h="full" w={256} transition="0.4s ease-in-out" zIndex={100} _dark={{ bg: 'surface' }}>
-				<Grid templateRows="auto 1fr auto" h="full">
-					<GridItem p={6}>
-						<Flex align="center" gap={2} color="accent-1">
-							<Icon as={FiBox} boxSize={6} />
+					<Flex align="center" gap={2} color="accent-1">
+						<Icon as={FiBox} boxSize={6} />
 
-							<Text fontSize="lg" fontWeight="semibold">
-								TSVJ CENTER
-							</Text>
+						<Text fontSize="lg" fontWeight="semibold">
+							TSVJ CENTER
+						</Text>
+					</Flex>
+				</Flex>
+
+				<Flex display={{ base: 'none', md: 'flex' }} justify="end" align="center" gap={3}>
+					{(!session || (session && session.user.role === 'User')) && (
+						<Flex align="center" gap={8}>
+							<NextLink href="/" passHref>
+								<Link as="span" active={router.pathname.includes('') ? 1 : 0}>
+									Home
+								</Link>
+							</NextLink>
+
+							<NextLink href="/blogs" passHref>
+								<Link as="span" active={router.pathname.includes('blogs') ? 1 : 0}>
+									Blogs
+								</Link>
+							</NextLink>
+
+							<NextLink href="/company" passHref>
+								<Link as="span" active={router.pathname.includes('company') ? 1 : 0}>
+									Company
+								</Link>
+							</NextLink>
+
+							<NextLink href="/contact" passHref>
+								<Link as="span" active={router.pathname.includes('contact') ? 1 : 0}>
+									Call Us
+								</Link>
+							</NextLink>
 						</Flex>
-					</GridItem>
+					)}
 
-					<GridItem p={session && session.user.role === 'Admin' ? 3 : 6}>
-						{session && session.user.role === 'Admin' && (
-							<Flex direction="column">
-								{links.admin.map((data, index) => (
-									<NextLink href={`/${data.pathname}`} passHref key={index}>
-										<Flex bg={router.pathname.includes(data.pathname) ? 'canvas-1' : 'transparent'} justify="space-between" align="center" gap={6} borderRadius={12} px={3} h="48px" color={router.pathname.includes(data.pathname) ? 'accent-1' : 'accent-3'} transition=".4s" _hover={{ color: 'accent-1' }} onClick={onSidebarClose}>
-											<Flex align="center" gap={3}>
-												<Icon as={data.icon} boxSize={4} />
+					{session ? (
+						<Flex align="center" gap={3}>
+							<IconButton variant="ghost" icon={colorModeIcon} onClick={toggleColorMode} />
 
-												<Text fontSize="sm" fontWeight="medium" textTransform="capitalize">
-													{data.pathname}
-												</Text>
-											</Flex>
-										</Flex>
-									</NextLink>
-								))}
-							</Flex>
-						)}
-
-						{(!session || (session && session.user.role !== 'Admin')) && (
-							<Flex direction="column" gap={6}>
-								{session && session.user.role === 'Tenant' ? (
-									<>
-										{links.user.map((link, index) => (
-											<NextLink href={`/${link.pathname}`} passHref key={index}>
-												<Link as="span" display="block" active={router.pathname.includes(link.pathname) ? 1 : 0}>
-													{link.name}
-												</Link>
-											</NextLink>
-										))}
-									</>
-								) : (
-									<>
-										{links.default.map((link, index) => (
-											<NextLink href={`/${link.pathname}`} passHref key={index}>
-												<Link as="span" display="block" size="lg" active={router.pathname.includes(link.pathname) ? 1 : 0}>
-													{link.name}
-												</Link>
-											</NextLink>
-										))}
-									</>
-								)}
-							</Flex>
-						)}
-					</GridItem>
-
-					<GridItem p={6}>
-						{session ? (
-							<Flex flex={1} gap={3}>
-								<Flex align="center" gap={3}>
+							<Menu>
+								<MenuButton>
 									<Avatar name={session.user.name} src={session.user.image} />
+								</MenuButton>
 
-									<Text fontSize="sm" fontWeight="medium" color="accent-1" noOfLines={1}>
-										{session.user.name}
-									</Text>
-								</Flex>
+								<MenuList w={256}>
+									<MenuItem>
+										<Flex align="center" gap={3}>
+											<Avatar name={session.user.name} src={session.user.image} />
 
-								<IconButton variant="ghost" size="xs" color="accent-1" icon={<FiLogOut size={16} />} onClick={() => signOut()} />
-							</Flex>
-						) : (
-							<Button colorScheme="brand" size="lg" w="full" onClick={() => signIn('google')}>
+											<Text color="accent-1" noOfLines={1}>
+												{session.user.name}
+											</Text>
+										</Flex>
+									</MenuItem>
+
+									<MenuDivider />
+
+									<MenuItem icon={<FiLogOut size={16} />} onClick={() => signOut()}>
+										Log out
+									</MenuItem>
+								</MenuList>
+							</Menu>
+						</Flex>
+					) : (
+						<Flex align="center" gap={3}>
+							<IconButton variant="ghost" icon={colorModeIcon} onClick={toggleColorMode} />
+
+							<Button colorScheme="brand" onClick={() => signIn('google')}>
 								Sign in
 							</Button>
-						)}
-					</GridItem>
-				</Grid>
-			</chakra.aside>
-		</>
+						</Flex>
+					)}
+				</Flex>
+
+				<Flex display={{ base: 'flex', md: 'none' }} align="center" gap={3}>
+					<IconButton variant="ghost" icon={colorModeIcon} onClick={toggleColorMode} />
+					<IconButton variant="outline" color="accent-1" icon={<FiMenu size={16} />} onClick={onSidebarOpen} />
+				</Flex>
+			</Flex>
+		</chakra.header>
 	)
 }
 
-export default Sidebar
+export default Header
